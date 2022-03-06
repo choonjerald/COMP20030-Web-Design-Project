fb46f13a4df78405d514e31a557b32d0b47b6bc1
choonjerald <choonjerald@gmail.com>
2022-03-06 09:35:40 +0800

    first commit

---
+++ actionstarbottle.js
@@ -0,0 +1,25 @@
+var small = document.getElementById("s");
+var medium = document.getElementById("m");
+var large = document.getElementById("l");
+var value = document.getElementById("price");
+
+small.onclick = function(){
+  value.innerHTML = '€25';
+  small.classList.add("active");
+  medium.classList.remove("active");
+  large.classList.remove("active");
+}
+
+medium.onclick = function(){
+  value.innerHTML = '€30';
+  small.classList.remove("active");
+  medium.classList.add("active");
+  large.classList.remove("active");
+}
+
+large.onclick = function(){
+  value.innerHTML = '€35';
+  small.classList.remove("active");
+  medium.classList.remove("active");
+  large.classList.add("active");
+}
